ba9a5bb0e23e459a2d2386b10f2436028f44f6cc
lperezcas16 <lau.manu2@gmail.com>
2020-06-10 22:20:33 +0000

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,6 @@ int main(void)
   printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
   printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
   printf("Size of a float: %ld byte(s)\n", sizeof(float));
-
  return (0);
 }
 
