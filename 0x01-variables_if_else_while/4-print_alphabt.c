c964355d5a3e5008f2f656d26643f08b7f9907cf
lperezcas16 <lau.manu2@gmail.com>
2020-06-11 22:12:49 +0000

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio-h>
+/**
+*main - Entry
+*
+*
+*Return: Always 0
+*/
+int main(void)
+{
+	char letra;
+	for (letra = "a"; letra <= 'z'; letra++)
+	{
+		if (letra == 'q' || i == 'e')
+		{
+			letra++;
+		}
+	putchar (letra);
+	}
+	putchar ('\n');
+	return (0);
+}
