a7636511f0e780c22a1fa019d074772f70c1103f
lperezcas16 <lau.manu2@gmail.com>
2020-06-15 15:57:16 +0000

    Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - main
+*
+*Return: Always 0
+*/
+
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
