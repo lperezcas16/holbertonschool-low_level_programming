e29cdd7f98770f75e3fde9db7bc398a4890c9171
lperezcas16 <lau.manu2@gmail.com>
2020-06-16 18:08:51 +0000

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,26 +2,69 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - Funtion
- * @A: variable
+ * print_to_98 - prints natural numbers from n to 98
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * @n: starting point
+ * Return: void
  */
-void print_to_98(int A)
+void print_to_98(int n)
 {
+	if (n > 98)
+		while (n > 98)
+			_putnumber(n--);
+	if (n < 98)
+		while (n < 98)
+			_putnumber(n++);
+	if (n == 98)
+		_putnumber(98);
+}
 
-	while (A != 98)
+/**
+ * _putnumber - puts number to output, formatted with a comma
+ *
+ * @n: number to print to output
+ * Return: void
+ */
+void _putnumber(int n)
+{
+	if (n < 0)
 	{
-		printf("%i, ", A);
-		if (A > 98)
-		{
-			A--;
-		}
-		else if (A < 98)
-		{
-			A++;
-		}
+		_putchar('-');
 	}
-	printf("%i\n", 98);
+	if (n == 98)
+	{
+		_putchar('9');
+		_putchar('8');
+		_putchar(10);
+	}
+	if (n == -98)
+	{
+		_putchar('9');
+		_putchar('8');
+		_putchar(',');
+		_putchar(' ');
+	}
+	if (n <= 0)
+		n = (n - (n * 2));
+	if (n < 10)
+	{
+		_putchar(n + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	else if (n >= 10 && n != 98 && n < 100)
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	else if (n >= 100)
+	{
+		_putchar(n / 100 + '0');
+		_putchar(n / 10 % 10 + '0');
+		_putchar(n % 10 + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
 }
