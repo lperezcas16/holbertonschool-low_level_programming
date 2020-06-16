d86f756ff1a6ae74367e0a1708f1ce8a3ea3c8a0
lperezcas16 <lau.manu2@gmail.com>
2020-06-16 17:55:56 +0000

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,28 +1,56 @@
 #include "holberton.h"
 
 /**
-*times_table - Funtion
-*
-*Return: it's void
-*/
-void number(int n)
+ * times_table - prints times table up to 9
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int i, j;
+
+	i = 0;
+	while (i < 10)
+	{
+		_putchar('0');
+		j = 1;
+		while (j < 10)
+		{
+			putformatnumber(i * j++);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
+
+/**
+ * putformatnumber - check the code for Holberton School students.
+ *
+ * @n: number to print formatted
+ * Return: Always 0.
+ */
+void putformatnumber(int n)
 {
 	if (n < 10)
-	
+	{
 		_putchar(',');
 		_putchar(' ');
 		_putchar(' ');
 		_putchar(n + '0');
-	else if (n >= 10 && n <
-	
-}
-void times_table(void)
-{
-	int n, i;
-
-	for (i = 0; i < 10; i++)
+	}
+	else if (n >= 10 && n < 100)
 	{
-	printf("%d ", (2 * i));
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n / 100 + '0');
+		_putchar(n / 10 % 10 + '0');
+		_putchar(n % 10 + '0');
 	}
 }
-
