f9d2b7adf9d8b7bd6d7186ffb9073514fcefbc7d
lperezcas16 <lau.manu2@gmail.com>
2020-06-16 16:46:08 +0000

    actualizacion

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+*times_table - Funtion
+*
+*Return: it's void
+*/
+void number(int n)
+{
+	if (n < 10)
+	
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n + '0');
+	else if (n >= 10 && n <
+	
+}
+void times_table(void)
+{
+	int n, i;
+
+	for (i = 0; i < 10; i++)
+	{
+	printf("%d ", (2 * i));
+	}
+}
+
