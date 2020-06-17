61e418ccc39190be796c54126b9fc7aea0fd766a
lperezcas16 <lau.manu2@gmail.com>
2020-06-17 04:40:46 +0000

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,44 +6,24 @@
  */
 void print_times_table(int n)
 {
-	int i;
-	int j;
+	int t = 0, i, j;
 
-	if ((n > 15) || (n < 0))
-		return;
-
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
+	if ((n < 15) && (n > 0))
+		for (i = 0; i <= n; i++)
 		{
-			if ((i * j) < 10)
+			for (j = 0; j <= n; j++)
 			{
-				if (j != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-				}
+				t = i * j;
+				(t / 100 == 0) ?
+					_putchar(' ') :
+					_putchar(t / 100 + 48);
+				(t / 10 == 0) ?
+					_putchar(' ') :
+					_putchar((t / 10) % 10 + 48);
+				_putchar(t % 10 + 48);
+				if  (j != n)
+					_putchar(','), _putchar(' ');
 			}
-			else if ((i * j) < 100)
-			{
-				_putchar(' ');
-				_putchar(i * j / 10 + '0');
-			}
-			else
-			{
-				_putchar(i * j / 100 + '0');
-				_putchar((i * j / 10) % 10 + '0');
-			}
-			_putchar(i * j % 10 + '0');
-			if (j == n)
-			{
-				_putchar('\n');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar('\n');
 		}
-	}
 }
