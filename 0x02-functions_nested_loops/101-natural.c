355d4a6e09e1d674d564815f301fb455242a3913
lperezcas16 <lau.manu2@gmail.com>
2020-06-17 02:53:38 +0000

    101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-	int total;
+	int total = 0;
 	int i;
 
 	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0) && (i % 6 == 0))
+		if ((i % 3 == 0) || (i % 5 == 0))
 			total += i;
 	}
 	printf("%d\n", total);
