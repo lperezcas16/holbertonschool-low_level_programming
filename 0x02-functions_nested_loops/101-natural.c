9b73ad5a814afeaa1c60f291cb5e4ff808993909
lperezcas16 <lau.manu2@gmail.com>
2020-06-17 00:57:32 +0000

    101 executable

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,12 +9,13 @@
 int main(void)
 {
 	int total;
+	int i;
 
-		for (int i = 0; i < 1024; i++)
-		{
-			if ((i % 3 == 0) && (i % 5 == 0))
-				total += i;
-		}
-				printf("%d", total);
+	for(i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			total += i;
+	}
+		printf("%d", total);
 	return (0);
 }
