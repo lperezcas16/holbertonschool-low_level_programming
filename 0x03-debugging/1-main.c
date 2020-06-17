c8a67b398e589a9cb12fef3537815d491a6f5a82
lperezcas16 <lau.manu2@gmail.com>
2020-06-17 19:53:56 +0000

    readme - header called holberton - main 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - funtion
+*
+*Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming ::(\n");
+
+	i = 0;
+	
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
