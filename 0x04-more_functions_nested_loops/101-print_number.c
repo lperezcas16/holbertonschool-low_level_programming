cb7b79376b7a1398ed03de49a838ab546e3c4c7b
lperezcas16 <lau.manu2@gmail.com>
2020-06-18 19:01:08 +0000

    holberton header

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,38 +1,37 @@
 #include "holberton.h"
 
 /**
- * print_number - check for numbers
- *
- * @n: Value of input number
+ * print_number - function that prints an integer..
+ * @n: input value to check
+ * Return: nothing.
  */
+
 void print_number(int n)
 {
-	unsigned int c_digit, ten_pow, a, print_n;
-	unsigned int num = n;
+	unsigned int num, num2;
+	int i;
+	int aux = 1;
 
-	c_digit = 0;
 	if (n < 0)
 	{
+		n = n * -1;
 		_putchar('-');
-		num = num * -1;
 	}
-	while (n != 0)
+	num = n;
+	num2 = num;
+	if (num > 9)
 	{
-		n = n / 10;
-		c_digit++;
-	}
-	if (c_digit >= 1)
-	{
-		ten_pow = 1;
-		for (a = 1; a < c_digit; a++)
-			ten_pow *= 10;
-		while (ten_pow >= 1)
+		while (num >= 10)
 		{
-			print_n = (num / ten_pow) % 10;
-			_putchar(print_n + '0');
-			ten_pow = ten_pow / 10;
+			aux = aux * 10;
+			num = num / 10;
 		}
+		_putchar((num2 / aux) + '0');
+		aux = aux / 10;
+
+		for (i = aux; i >= 1; i = i / 10)
+			_putchar((num2 / i) % 10 + '0');
 	}
 	else
-		_putchar('0');
+		_putchar(num + '0');
 }
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,3 +1,4 @@
+void print_number(int n);
 void print_triangle(int size);
 void print_square(int size);
 void print_diagonal(int n);
