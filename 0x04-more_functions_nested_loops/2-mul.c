bf0d14efc6d3670ebb4fd418e9b16286db8a9946
lperezcas16 <lau.manu2@gmail.com>
2020-06-18 17:03:07 +0000

    all

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+*mul - Funtion
+*
+*@a: variable
+*@b: variable
+*Return: is 0
+*/
+
+int mul(int a, int b)
+{
+	
+	a*=b;
+	return (a);
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,3 +1,10 @@
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+int _putchar(char c);
+void print_numbers(void);
 int mul(int a, int b);
 int _isdigit(int c);
 int _isupper(int c);
