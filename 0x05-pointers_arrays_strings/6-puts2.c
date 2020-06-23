07392228e0b772dc729f1fd02961903a12cc2ec1
lperezcas16 <lau.manu2@gmail.com>
2020-06-23 18:44:45 +0000

    Just Par

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,10 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; *(str + i) != '\0'; i = i + 2)
-	{
-		_putchar(*(str + i));
-	}
+	while (str[i])
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+
 	_putchar('\n');
 }
