3e970756f4671ec32f0a9b8a9297d4b9440335da
lperezcas16 <lau.manu2@gmail.com>
2020-06-26 00:37:01 +0000

    Print numbers

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ */
+void print_number(int n)
+{
+	unsigned int b;
+
+	if (n < 0)
+	{
+		b = -n;
+		_putchar('-');
+	}
+	else
+	{
+		b = n;
+	}
+	if (b / 10)
+	{
+		print_number(b / 10);
+	}
+	_putchar((b % 10) + '0');
+}
