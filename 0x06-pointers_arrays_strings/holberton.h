1744f218aed9695a8b4012eb0be23786f3f6488d
lperezcas16 <lau.manu2@gmail.com>
2020-06-26 02:04:07 +0000

    Header update

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
