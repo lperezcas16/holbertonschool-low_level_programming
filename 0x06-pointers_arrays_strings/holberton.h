699c28f26134398f7507e68e09f07a8edc1346f8
lperezcas16 <lau.manu2@gmail.com>
2020-06-27 17:18:04 +0000

    Empty files

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+void print_buffer(char *b, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
 char *_strcat(char *dest, char *src);
