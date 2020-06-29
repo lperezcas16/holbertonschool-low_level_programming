0ffb5a035679abe7d39809f69c12d93574d257c3
lperezcas16 <lau.manu2@gmail.com>
2020-06-29 23:24:09 +0000

    a function that searches a string for any of a set of bytes. :grinning:

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,11 +12,14 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int i = 0, p = 0, j = 0;
 
 	for (; s[i]; i++)
+	{
 		for (; accept[j]; j++)
 			if (accept[j] == s[i])
 			{
 				p += 1;
+	
 			}
+	}
 	return (p);
 
 }
