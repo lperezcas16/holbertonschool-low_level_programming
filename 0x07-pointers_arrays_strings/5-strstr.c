ad2b403a4dbf2eafe27797fd145e077eea10edc9
lperezcas16 <lau.manu2@gmail.com>
2020-06-30 03:43:07 +0000

    function that locates a substring. :weary:

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,11 +1,32 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
- * _strstr -
+ * _strstr - Funtion
+ * @haystack: memory area
+ * @needle: another memory area
  *
- * Return: 
+ * Return: always 0
  */
 char *_strstr(char *haystack, char *needle)
 {
+	char *a, *c;
+	
+	while (*haystack)
+	{
+		c = haystack;
+		a = needle;
 
+		while (*a == *haystack && *a)
+		{
+			haystack++, a++;
+			
+		}
+
+		if (*a == '\0')
+			return (c);
+		haystack = c + 1;
+	
+	}
+	return (0);
 }
