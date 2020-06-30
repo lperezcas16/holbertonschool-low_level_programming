2b274991f1d993ea43a5bed54047d02193268cfc
lperezcas16 <lau.manu2@gmail.com>
2020-06-30 03:56:30 +0000

    function that prints the chessboard.:thumbsup:

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,11 +1,25 @@
 #include "holberton.h"
 
 /**
- * print_chessboard -
+ * print_chessboard - Funtion
+ * @a: Matrix
  *
  * Return: 
  */
 void print_chessboard(char (*a)[8])
 {
+	int c, f ,g;
+
+	g = sizeof(a);
+
+	for (c = 0; c < g; c++)
+	{
+		for (f = 0; f < g; f++)
+		{
+			_putchar(a[c][f]);
+		}
+		_putchar('\n');
+	}
+
 
 }
