bb54bac1b7b6d6ab124e506f6b87c036cae87b8c
lperezcas16 <lau.manu2@gmail.com>
2020-07-01 18:48:02 +0000

    :heart_eyes:

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,11 +1,19 @@
 #include "holberton.h"
 
 /**
- * _strlen_recursion -
+ * _strlen_recursion - Funtion
  *
- * Return: 
+ * @s: Variable that contains the String
+ *
+ * Return: Int
  */
 int _strlen_recursion(char *s)
 {
 
+
+	if (*s == '\0')
+		return (0);
+
+	s++;
+	return (_strlen_recursion(s) + 1);
 }
--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,11 +1,17 @@
 #include "holberton.h"
 
 /**
- * factorial -
+ * factorial - Funtion
  *
- * Return: 
+ * @n: Variable
+ *
+ * Return: int
  */
 int factorial(int n)
 {
-
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		return (-1);
+	return (n * factorial(n - 1));
 }
