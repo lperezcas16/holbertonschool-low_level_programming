97826515afd579b379973b3e8a389f669c86263f
lperezcas16 <lau.manu2@gmail.com>
2020-07-01 16:04:23 +0000

    :abc:

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,22 @@
 #include "holberton.h"
 
 /**
- * _puts_recursion -
+ * _puts_recursion - Funtion
  *
- * Return: 
+ * @s: Variable
+ *
  */
 void _puts_recursion(char *s)
 {
+
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 
 }
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 
 /**
- * _print_rev_recursion -
+ * _print_rev_recursion - Funtion
  *
- * Return: 
+ * @s: char
+ *
  */
 void _print_rev_recursion(char *s)
 {
