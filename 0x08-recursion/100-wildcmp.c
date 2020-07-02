5e93c14ccda2ec5a2017932e557e4a44b6f59e57
lperezcas16 <lau.manu2@gmail.com>
2020-07-02 02:47:26 +0000

    function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0 .:smiling_imp:

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,11 +1,27 @@
 #include "holberton.h"
 
 /**
- * wildcmp -
+ * wildcmp - Funtion
  *
- * Return: 
+ * @s1: Variable
+ * @s2: Variable
+ *
+ * Return: Int
  */
 int wildcmp(char *s1, char *s2)
 {
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
 
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	return (0);
 }
