2c71c95b0a90d2a06a4c427e95df4a14901dd053
lperezcas16 <lau.manu2@gmail.com>
2020-07-05 03:35:39 +0000

    Remove some files

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -8,10 +8,9 @@
 
 void _puts(char *str)
 {
-		int i;
+	int i;
 
-			for (i = 0; str[i]; i++)
-						_putchar(str[i]);
-
-				_putchar('\n');
+	for (i = 0; str[i]; i++)
+		_putchar(str[i]);
+	_putchar('\n');
 }
