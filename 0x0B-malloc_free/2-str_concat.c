ad98abe2db0719fac372cb4935edfc755ebdfb9d
lperezcas16 <lau.manu2@gmail.com>
2020-07-08 03:58:19 +0000

    a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,11 +1,37 @@
 #include "holberton.h"
 
 /**
- * str_concat -
+ * str_concat - Funtion}
  *
- * Return: 
+ * @s1: arry
+ * @s2: arry
+ *
+ * Return: Usually 0
  */
 char *str_concat(char *s1, char *s2)
 {
+	char *pnt = NULL;
+	int size, size2;
+
+	for (size = 1; s1[size - 1]; size++)
+	;
+	for (size2 = 1; s2[size2 - 1]; size2++)
+	;
+	size = size + size2 - 1;
+	pnt = malloc(sizeof(char) * size);
+
+	if (!s1 || !s2)
+		return (NULL);
+
+	for (size = 0; s1[size]; size++)
+		pnt[size] = s1[size];
+
+	for (size2 = 0; s2[size2]; size2++)
+	{
+		pnt[size + size2] = s2[size2];
+	}
+	pnt[size + size2] = '\0';
+	return (pnt);
+
 
 }
