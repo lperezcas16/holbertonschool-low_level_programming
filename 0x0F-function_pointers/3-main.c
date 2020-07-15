9ff9f55b7aa1f3d982e75b533b9ceeb8141d3c8f
lperezcas16 <lperezcas@unbosque.edu.co>
2020-07-15 23:09:40 +0000

    fix 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,42 +5,42 @@
 /**
  * main - Principal
  *
- * @argc: count arguments
- * @argv: array
+ * @ac: count arguments
+ * @ag: array
  *
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int ac, char *ag[])
 {
 	int a;
 	int b;
 
-	if (argc != 4)
+	if (ac != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if ((*argv[2] != '+' &&
-		*argv[2] != '-' &&
-		*argv[2] != '/' &&
-		*argv[2] != '*' &&
-		*argv[2] != '%')
-		|| argv[2][1] != '\0')
+	if ((*ag[2] != '+' &&
+		*ag[2] != '-' &&
+		*ag[2] != '/' &&
+		*ag[2] != '*' &&
+		*ag[2] != '%')
+		|| ag[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+	a = atoi(ag[1]);
+	b = atoi(ag[3]);
 
-	if (b == 0 && (argv[2][0] == '/' || argv[2][0]))
+	if (b == 0 && (ag[2][0] == '/' || ag[2][0]))
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	printf("%d\n", (get_op_func(argv[2])(a, b)));
+	printf("%d\n", (get_op_func(ag[2])(a, b)));
 	return (0);
 
 }
