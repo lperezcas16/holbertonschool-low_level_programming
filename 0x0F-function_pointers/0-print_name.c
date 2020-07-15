17ab0efab9542b50d56be6ceb0f73d6c13628ce7
lperezcas16 <lperezcas@unbosque.edu.co>
2020-07-15 16:26:38 +0000

    Update Files

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,15 @@
 #include "function_pointers.h"
 
 /**
- * print_name -
+ * print_name - Prints a name
  *
- * Return: 
+ * @name: String
+ * @f: Pointer to funtion
+ *
  */
 void print_name(char *name, void (*f)(char *))
 {
 
+	if (name && f)
+		f(name);
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,11 +1,24 @@
 #include "function_pointers.h"
 
 /**
- * int_index -
+ * int_index - Search an integer
  *
- * Return: 
+ * @array: Array of the numbers
+ * @size: Size of the array
+ * @cmp: pointer of the funtion
+ *
+ * Return: An integer
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
+
+	if (size > 0 && array && cmp)
+	{
+		for (i = 0; i < size; i++)
+			if (cmp(array[i] != 0)
+					return (i);
+	}
+	return (-1);
 
 }
