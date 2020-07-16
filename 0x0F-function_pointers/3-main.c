9c59e75dd66c01039e2972ddb36d18c448034424
Laura Pérez Castillo <57758299+lperezcas16@users.noreply.github.com>
2020-07-16 10:27:02 -0500

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,34 +10,34 @@
  *
  * Return: Always 0
  */
-int main(int ac, char *ag[])
+int main(int arc, char *arg[])
 {
 	int num1, num2, (*operate)(int, int);
 
-	if (ac != 4)
+	if (arc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if ((*ag[2] != '+' &&
-		*ag[2] != '-' &&
-		*ag[2] != '/' &&
-		*ag[2] != '*' &&
-		*ag[2] != '%')
-		|| ag[2][1] != '\0')
+	if ((*arg[2] != '+' &&
+		*arg[2] != '-' &&
+		*arg[2] != '/' &&
+		*arg[2] != '*' &&
+		*arg[2] != '%')
+		|| arg[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	num1 = atoi(ag[1]);
-	num2 = atoi(ag[3]);
-	if ((*ag[2] == '/' || *ag[2] == '%') && (num2 == 0))
+	num1 = atoi(arg[1]);
+	num2 = atoi(arg[3]);
+	if ((*arg[2] == '/' || *arg[2] == '%') && (num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	operate = get_op_func(ag[2]);
+	operate = get_op_func(arg[2]);
 	printf("%d\n", operate(num1, num2));
 	return (0);
 
