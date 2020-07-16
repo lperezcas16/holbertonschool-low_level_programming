5efcc42b36965b9925698e2cf6b9440499fd24e7
lperezcas16 <lperezcas@unbosque.edu.co>
2020-07-16 00:27:54 +0000

    fix 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -30,13 +30,14 @@ int main(int ac, char *ag[])
 		exit(99);
 	}
 
-	if ((*ag[2] == '/' || *ag[2] == '%') && (ag[3] == 0))
+	num1 = atoi(ag[1]);
+	num2 = atoi(ag[3]);
+	if ((*ag[2] == '/' || *ag[2] == '%') && (num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	num1 = atoi(ag[1]);
-	num2 = atoi(ag[3]);
+	
 	operate = get_op_func(ag[2]);
 	printf("%d\n", operate(num1, num2));
 	return (0);
