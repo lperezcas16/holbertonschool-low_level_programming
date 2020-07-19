83b7a42c44b59d5fd09028c3c6b397b2c6a0213c
lperezcas16 <lperezcas@unbosque.edu.co>
2020-07-19 23:14:39 +0000

    Co-authored-by: Marianella Monroy Ortiz <66291974+marianellamonroyortizhb@users.noreply.github.com> Co-authored-by: Gustavo Adolfo Mejía Sánchez <gamez.live@gmail.com>

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct funs
+{
+	char fmt;
+	void (*f)(va_list list);
+} filter;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
