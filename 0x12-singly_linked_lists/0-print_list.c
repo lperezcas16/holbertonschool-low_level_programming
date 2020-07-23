09e5acb6227baa97de4188c76dc72744831989cc
lperezcas16 <lperezcas@unbosque.edu.co>
2020-07-23 22:36:29 +0000

    [upload] principal files

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - Funtion 
+ * @h: list
+ *
+ * Return: size_t
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	for (i = 0; h != NULL; i++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (i);
+}
