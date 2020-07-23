798955c3b865ad66bb5312e82ba3640d57ff18a0
lperezcas16 <lperezcas@unbosque.edu.co>
2020-07-23 22:57:53 +0000

    [fix] print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,13 +10,13 @@ size_t print_list(const list_t *h)
 {
 	size_t i = 0;
 
-	for (i = 0; h != NULL; i++)
+	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%i] %s\n", h->len, h->str);
-		h = h->next;
+		h = h->next, i++;
 	}
 	return (i);
 }
