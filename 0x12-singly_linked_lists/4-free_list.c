3adfac961216ffeefb13bfc293f5ee486c00983c
lperezcas16 <lperezcas@unbosque.edu.co>
2020-07-24 04:09:55 +0000

    [Update] file 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,11 +1,20 @@
 #include "lists.h"
 
 /**
- * free_list -
+ * free_list - Functinos
+ * @head: list_t
  *
- * Return: 
+ * Return: 0
  */
 void free_list(list_t *head)
 {
+	list_t *ptr;
 
+	while (head)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr->str);
+		free(ptr);
+	}
 }
