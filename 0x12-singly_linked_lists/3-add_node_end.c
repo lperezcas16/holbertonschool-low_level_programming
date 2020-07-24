e460ff0c54dac17e810d650dd5f3915fe0d4d88f
lperezcas16 <lperezcas@unbosque.edu.co>
2020-07-24 02:50:56 +0000

    [Update] file 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,11 +1,34 @@
 #include "lists.h"
 
 /**
- * add_node_end -
+ * add_node_end - Function
+ * @head: first nodo
+ * @str: String 
  *
- * Return: 
+ * Return: list_t
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *new_nodo = malloc(sizeof(list_t)), *last = NULL;
+	char *ptr = strdup(str);
+
+	if(!new_nodo)
+		return (NULL);
+
+	new_nodo->str = ptr;
+	new_nodo->len = strlen(str);
+	new_nodo->next = NULL;
+
+	last = *head;
+	if (!last)
+		*head = new_nodo;
+	else
+	{
+		while (last->next)
+			last = last->next;
+		last->next = new_nodo;
+	}
+	return (*head);
+	
 
 }
