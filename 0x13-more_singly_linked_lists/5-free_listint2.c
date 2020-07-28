5ee23df9d80d4b750223710219c5dc5cc01aed28
lperezcas16 <lperezcas@unbosque.edu.co>
2020-07-28 18:34:39 +0000

    [fix] file 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 
 /**
- * free_listint2 -
+ * free_listint2 - Funtions that free a list head = NULL
+ * @head: pointer
  *
- * Return: 
+ * Return: noting 
  */
 void free_listint2(listint_t **head)
 {
-listint_t *tmp;
+	listint_t *tmp;
 
 	if (!head)
 		return;
