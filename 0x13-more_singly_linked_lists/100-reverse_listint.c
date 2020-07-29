0b9cb37a79f39129dba3fa4aed89bca33c603646
lperezcas16 <lperezcas@unbosque.edu.co>
2020-07-29 00:10:39 +0000

    [update] files 6 - 7 - gitignore

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,11 +1,13 @@
 #include "lists.h"
 
 /**
- * reverse_listint -
+ * reverse_listint - Funtion Reverse the list
+ * @head: head of linked list
  *
- * Return: 
+ * Return: list
  */
 listint_t *reverse_listint(listint_t **head)
 {
+	
 
 }
