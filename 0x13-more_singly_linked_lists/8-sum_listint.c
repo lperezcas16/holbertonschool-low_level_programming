c52fc7a62e9548e35dbf39f001a8bb14aa351a42
lperezcas16 <lperezcas@unbosque.edu.co>
2020-07-29 02:41:31 +0000

    [update] file 8 - 9

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,11 +1,19 @@
 #include "lists.h"
 
 /**
- * sum_listint -
+ * sum_listint - Function that sum lists
+ * @head: head nodo
  *
- * Return: 
+ * Return: integer
  */
 int sum_listint(listint_t *head)
 {
+	listint_t *new = head;
+	int sum;
 
+	while (new)
+	{
+		sum += new->n, new = new->next;
+	}
+	return (sum);
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,11 +1,51 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index -
+ * insert_nodeint_at_index - Function insert node
+ * @head: head nodo
+ * @idx: index
+ * @n: integer
  *
- * Return: 
+ * Return: listint
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
-{
+{	
+	listint_t *new;
+	listint_t *actual = *head;
+	unsigned int pos = 0;
 
-}
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL && idx > 0)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
+	for (; pos < idx - 1; pos++)
+	{
+		actual = actual->next;
+		if (actual == NULL && idx - pos > 0)
+		{
+			free(new);
+			return (NULL);
+		}
+	}
+
+	new->next = actual->next;
+	actual->next = new;
+
+	return (new);;
+}	
