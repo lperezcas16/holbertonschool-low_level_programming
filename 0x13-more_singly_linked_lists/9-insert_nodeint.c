74f235453f06b08908894b5c834a4595d9f99a9d
lperezcas16 <lperezcas@unbosque.edu.co>
2020-07-29 03:13:44 +0000

    [fix] 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,12 +10,12 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-		listint_t *new = malloc(sizeof(listint_t));
+	listint_t *new = malloc(sizeof(listint_t));
 	listint_t *actualNodo = *head;
 	unsigned int i = 0;
 
 	if (!new)
-		return (NULL);
+		return (free(new), NULL);
 
 	new->n = n;
 	new->next = NULL;
