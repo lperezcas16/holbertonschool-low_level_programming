8a579b923bb269d8315954aaec8d6f34e14c7c4d
lperezcas16 <lperezcas@unbosque.edu.co>
2020-07-29 03:03:52 +0000

    [update] file 9 -10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,11 +1,39 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index -
- *
- * Return: 
+ * delete_nodeint_at_index - Function that delete a nodo
+ * @head: nodo head
+ * @index: index
+ * Return: integer
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	unsigned int i;
+	listint_t *actual = *head;
+	listint_t *nodo;
 
+	if (!*head)
+		return (-1);
+	if (index != 0)
+	{
+		for (; actual != NULL; i++)
+		{
+			if (i  == index)
+			{
+				nodo = actual->next;
+				actual->next = nodo->next;
+				free(nodo);
+				return (1);
+			}
+			actual = actual->next;
+		}
+	}
+	else
+	{
+		*head = (*head)->next;
+		free(actual);
+		return (1); 
+	}
+	
+	return (-1);
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {	
 	listint_t *new = malloc(sizeof(listint_t));
-	listint_t *actual = *head;
+	listint_t *actualNodo = *head;
 	unsigned int i = 0;
 
 	if (!new)
@@ -35,16 +35,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	for (; i < idx - 1; i++)
 	{
-		actual = actual->next;
-		if (actual == NULL && idx - i > 0)
+		actualNodo = actualNodo->next;
+		if (actualNodo == NULL && idx - i > 0)
 		{
 			free(new);
 			return (NULL);
 		}
 	}
 
-	new->next = actual->next;
-	actual->next = new;
+	new->next = actualNodo->next;
+	actualNodo->next = new;
 
 	return (new);
 }	
