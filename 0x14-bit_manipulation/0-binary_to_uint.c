64657c7c218c982a2446ef4c6b12cb3d6bfd669e
lperezcas16 <lperezcas@unbosque.edu.co>
2020-07-30 23:29:00 +0000

    [update] file 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,11 +1,31 @@
 #include "holberton.h"
 
 /**
- * binary_to_uint -
+ * binary_to_uint - Function that convert into unsigned int
  *
- * Return: 
+ *@b: String
+ * Return: the unsigned number
  */
 unsigned int binary_to_uint(const char *b)
 {
+	int result = 0, count = 0, pow = 1;
+	
+	if (!b)
+		return (0);
 
+	while (b[count])
+		count++;
+	count--;
+	while (count >= 0)
+	{
+		if (b[count] != '1' && b[count] != '0')
+			return (0);
+
+		result += (b[count] - 48) * pow;
+		pow *= 2;
+		count--;
+	}
+
+	return (result);;
 }
+
