e858a58de90805bd7578c30ebe0e1ee363de5e98
lperezcas16 <lperezcas@unbosque.edu.co>
2020-08-04 18:50:56 +0000

    [update] file 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,22 @@
 #include "holberton.h"
 
 /**
- * create_file -
+ * create_file -Function creates a file
+ * @filename: file to create
+ * @text_content: the content of filename
  *
- * Return: 
+ * Return: Integer
  */
 int create_file(const char *filename, char *text_content)
 {
-
+	int new_open, new_write, size = 0;
+	
+	if (!filename || !text_content)
+		return (-1);
+	new_open = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0644 );
+	new_write = write(new_open, text_content, size);
+	if (new_open == -1 || new_write == -1)
+		return (-1);
+	close(new_open);	
+	return (1);	
 }
