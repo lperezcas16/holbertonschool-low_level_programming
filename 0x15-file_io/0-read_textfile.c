c5405daa659569b56a4979773c3ba3ec24a43ac1
lperezcas16 <lperezcas@unbosque.edu.co>
2020-08-04 17:18:36 +0000

    [update] principal files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - Funtion reads text file and prints it to the POXIS
+ *
+ * @filename: the file to read
+ * @letters: NUmber of letters
+ *
+ * Return: size
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int new_read, new_write, new_open;
+	char *file = malloc(sizeof(char) * letters);
+
+	if (filename == NULL || file == NULL)
+		return (0);
+	new_open = open(filename, O_RDONLY);
+	new_read = read(new_open, file, letters);
+	new_write = write(STDOUT_FILENO, file, new_read);
+
+	if (new_read == -1 ||
+		new_open == -1 ||
+			new_write == -1 ||
+				new_write != new_read)
+	{
+		return (free(file), 0);
+
+	}
+	free(file);
+	close(new_open);
+	return (new_write);
+
+}
