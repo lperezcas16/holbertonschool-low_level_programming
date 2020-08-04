2f50d3b01fdeef7a4785577443c67e499aeb5990
lperezcas16 <lperezcas@unbosque.edu.co>
2020-08-04 19:52:10 +0000

    [fix] betty style #1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 		while (text_content[size])
 			size++;
-	new_open = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	new_open = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 	new_write = write(new_open, text_content, size);
 
 	if (!filename || new_open == -1 ||
