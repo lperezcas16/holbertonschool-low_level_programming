f98cf28d3bb07a5f43728d6060914e3a73c7fdea
lperezcas16 <lperezcas@unbosque.edu.co>
2020-08-04 18:54:27 +0000

    [fix] file 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,13 +10,13 @@
 int create_file(const char *filename, char *text_content)
 {
 	int new_open, new_write, size = 0;
-	
+
 	if (!filename || !text_content)
 		return (-1);
-	new_open = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR );
+	new_open = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	new_write = write(new_open, text_content, size);
 	if (new_open == -1 || new_write == -1)
 		return (-1);
-	close(new_open);	
-	return (1);	
+	close(new_open);
+	return (1);
 }
