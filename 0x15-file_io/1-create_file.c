9cc4589ff89f30ce10ad6b6240194fd446bf58b1
lperezcas16 <lperezcas@unbosque.edu.co>
2020-08-04 19:40:21 +0000

    [fix] permission #1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 		while (text_content[size])
 			size++;
-	new_open = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	new_open = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
 	new_write = write(new_open, text_content, size);
 
 	if (!filename || new_open == -1 ||
