39c99a9501dc93d33bebf595ce27c152e4ac94e0
lperezcas16 <lperezcas@unbosque.edu.co>
2020-08-04 21:10:01 +0000

    prueba

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,6 +11,7 @@ int create_file(const char *filename, char *text_content)
 	int file;
 	int bytes = 0;
 	int write_t;
+	int i = 0;
 
 	if (filename == NULL) /*Verification of filename */
 		return (-1);
@@ -19,8 +20,17 @@ int create_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 
-	while (*(text_content + bytes))
+	if (text_content == NULL) /*Verification of text_content*/
+	{
+		close(file);
+		return (1);
+	}
+
+	while (*(text_content + i) != '\0')
+	{
 		bytes++;
+		i++;
+	}
 
 	write_t = write(file, text_content, bytes);  /*writing inside the new file */
 	if (write_t == -1)
