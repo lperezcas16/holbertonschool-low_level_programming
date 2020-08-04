d28ba457087bdfa977076d7e44e80818466f1914
lperezcas16 <lperezcas@unbosque.edu.co>
2020-08-04 21:30:11 +0000

    [update] file #2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,11 +1,25 @@
 #include "holberton.h"
 
 /**
- * append_text_to_file -
+ * append_text_to_file - Function append text at the end of a file
+ * @filename: Text to edit
+ * @text_content: new content
  *
- * Return: 
+ * Return: Integer
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int new_open, new_write, size = 0;
 
+	if (!filename)
+		return (-1);
+	if (!text_content)
+		while (text_content[size])
+			size++;
+	new_open = open(filename,O_WRONLY | O_APPEND);
+	new_write = write(new_open, text_content, size);
+	if (new_open == -1 || new_write == -1)
+		return (-1);
+	close(new_open);
+	return (1);
 }
