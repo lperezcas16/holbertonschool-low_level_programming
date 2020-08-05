130a9d015968501d975c997c2ef16ad915e21296
lperezcas16 <lperezcas@unbosque.edu.co>
2020-08-05 12:27:52 +0000

    [fix] buffer

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,8 +9,10 @@
 int main(int ac, char **av)
 {
 	int new_open, new_open2, new_write, new_read = 1, new_close;
-	char *buffer[1024];
+	char *buffer = malloc(sizeof(char) * 1024);
 
+	if (buffer == NULL)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	new_open = open(av[1], O_RDONLY);
@@ -18,10 +20,7 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	new_open2 = open(av[2],  O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (new_open2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	while (new_read)
 	{
 		new_read = read(new_open, buffer, 1024);
