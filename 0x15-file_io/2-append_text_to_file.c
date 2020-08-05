4f2eff1e8690c37f311e8e90200905a08bc8e4c3
lperezcas16 <lperezcas@unbosque.edu.co>
2020-08-05 00:37:51 +0000

    [fix] file #2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,15 +11,16 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int new_open, new_write, size = 0;
 
-	if (filename == NULL)
-		return (-1);
-	if (!text_content)
-		while (text_content[size])
-			size++;
-	new_open = open(filename, O_WRONLY | O_APPEND);
+	new_open = open(filename, O_RDWR | O_APPEND);
 	new_write = write(new_open, text_content, size);
-	if (new_open == -1 || new_write == -1)
+	if (!text_content)
+		return (1);
+	if (text_content[size])
+		size++;
+	if (new_open == -1 || !filename)
 		return (-1);
+	if (new_write == -1)
+		return (close(new_open), -1);
 	close(new_open);
 	return (1);
 }
