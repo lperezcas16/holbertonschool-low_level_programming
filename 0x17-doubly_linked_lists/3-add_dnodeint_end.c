55fa8baf8b6e055f675a5a68b7b7ad7b9f1ad7a8
lperezcas16 <lperezcas@unbosque.edu.co>
2020-09-05 14:15:02 -0500

    [update] #3 #7

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,7 +8,7 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *aux, *new = malloc(sizeof(dlistint_t));
+	dlistint_t *aux = NULL, *new = malloc(sizeof(dlistint_t));
 
 	if (!new)
 		return (NULL);
---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Function insert
+ * @h: header
+ * @idx: index
+ * @n: valueo date
+ *
+ * Return: dlistint_t
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new = NULL, *tmp = *h;
+	unsigned int i;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	if (!*h)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	while (tmp)
+	{
+		if (idx == i)
+		{
+			new->prev = tmp->prev;
+			new->next = tmp;
+			tmp->prev = new;
+			new->prev->next = new;
+			return (new);
+		}
+		if (!tmp->next && idx == i + 1)
+			return (add_dnodeint(h, n));
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
