0b07b63a90f21eb5af0c57a5cde0dac430707366
lperezcas16 <lperezcas@unbosque.edu.co>
2020-09-05 13:31:43 -0500

    [fix style] #0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,9 +2,8 @@
 
 /**
  * print_dlistint - Prints all the elements of a list
- * 
  * @h: header, first node  *
- * Return - size_t
+ * Return: size_t
  */
 size_t print_dlistint(const dlistint_t *h)
 {
