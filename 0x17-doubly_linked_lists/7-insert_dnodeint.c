29c053cd78813502d21b57ace97ca2c3a6ebd445
lperezcas16 <lperezcas@unbosque.edu.co>
2020-09-06 12:07:15 -0500

    [up]

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,32 +9,4 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp = NULL, *new = *h;
-	unsigned int i;
-
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-	if (!*h)
-		return (NULL);
-	tmp = malloc(sizeof(dlistint_t));
-	if (!tmp)
-		return (NULL);
-	tmp->n = n;
-	while (new)
-	{
-		if (idx == i)
-		{
-			tmp->prev = new->prev;
-			tmp->next = new;
-			new->prev = tmp;
-			tmp->prev->next = tmp;
-			return (tmp);
-		}
-		if (!new->next && idx == i + 1)
-			return (free(tmp), add_dnodeint(h, n));
-		new = new->next;
-		i++;
-	}
-	free(tmp);
-	return (NULL);
 }
