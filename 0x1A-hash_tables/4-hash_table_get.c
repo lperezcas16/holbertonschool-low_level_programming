ed8289255aaa3bd6957d624b02db806040dd57e3
lperezcas16 <lperezcas@unbosque.edu.co>
2020-10-04 21:36:53 -0500

    [update] #4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,4 +8,19 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int idx = 0;
+	hash_node_t *tmp = NULL;
+
+	if (!ht || !key)
+		return (NULL);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[idx];
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (0);
 }
