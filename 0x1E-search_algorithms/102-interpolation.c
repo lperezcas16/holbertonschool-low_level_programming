d839fc2c4b7a1bf2a0882fa80926b50e5af2d4ca
lperezcas16 <lperezcas@unbosque.edu.co>
2022-06-13 08:44:33 -0500

    [ADD] file 102, task 10

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,51 +1,51 @@
 #include "search_algos.h"
 
 /**
- * interpolation_search - searches for a value in an array of
- * integers using the Interpolation search algorithm
+ * interpolation_search - searches for a value in a sorted array of integers
+ * using the Interpolation search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size:  number of elements in array
+ * @value: value to search for
  *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
+ * Return: Index where value is located or -1
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t pos, low, high;
-	double f;
+	size_t pos, high, low;
 
-	if (array == NULL)
+	if (!array || size == 0)
 		return (-1);
 
-	low = 0;
-	high = size - 1;
+	low = 0, high = size - 1;
 
-	while (size)
+	while (low < high)
 	{
-		f = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
-		pos = (size_t)(low + f);
-		printf("Value checked array[%d]", (int)pos);
+		pos = low + (((double)(high - low) /
+			      (array[high] - array[low])) * (value - array[low]));
 
-		if (pos >= size)
+		if (pos > (size - 1))
 		{
-			printf(" is out of range\n");
-			break;
+			printf("Value checked array[%lu] is out of range\n", pos);
+			return (-1);
 		}
 		else
-		{
-			printf(" = [%d]\n", array[pos]);
-		}
+			printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 
-		if (array[pos] == value)
-			return ((int)pos);
+		if (pos == 0 && array[pos] > value)
+			return (-1);
 
-		if (array[pos] < value)
+		if (array[pos] > value)
+			high = pos - 1;
+		else if (array[pos] < value)
 			low = pos + 1;
 		else
-			high = pos - 1;
+			return (pos);
+	}
 
-		if (low == high)
-			break;
+	if (array[low] == value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", low, array[low]);
+		return (low);
 	}
 
 	return (-1);
